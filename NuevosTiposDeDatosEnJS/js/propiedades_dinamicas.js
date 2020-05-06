83c90e38f66715518b7757ec90c54ce0d3316a06
AlonsoAM <alonso.amoreno@live.com>
2020-05-06 11:20:42 -0500

    Agregando Archivos

--- NuevosTiposDeDatosEnJS/js/propiedades_dinamicas.js
+++ NuevosTiposDeDatosEnJS/js/propiedades_dinamicas.js
@@ -1,1 +1,13 @@
-alert("Hola que tal")
+aleatorio = Math.round((Math.random() * 100) + 5)
+
+const objetoUsuarios = {
+    [`id_${aleatorio}`]: "Valor aleatorio"
+}
+
+const usuarios = ['Alonso', 'Irma', 'Miguel', 'Kala', 'Dominic']
+
+console.log(objetoUsuarios)
+
+usuarios.forEach((usuario, index) => objetoUsuarios[`id_${index}`] = usuario)
+
+console.log(objetoUsuarios)
