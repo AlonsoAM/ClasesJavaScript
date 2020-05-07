1140a637cd0c436fc7d57ac638ceaf2c81341c9d
AlonsoAM <alonso.amoreno@live.com>
2020-05-06 22:51:09 -0500

    Agregando Archivos

--- NuevosTiposDeDatosEnJS/js/json.js
+++ NuevosTiposDeDatosEnJS/js/json.js
@@ -15,4 +15,26 @@ console.log(json)
 
 console.log(JSON)
 
-console.log(JSON.parse)
+console.log(JSON.parse("[1,2,3]"))
+
+console.log(JSON.parse("{}"))
+
+console.log(JSON.parse("true"))
+
+console.log(JSON.parse("19"))
+
+//console.log(JSON.parse("Hola Mundo"));
+
+console.log(JSON.parse("null"));
+
+//console.log(JSON.parse("undefined"));
+
+console.log(JSON.stringify("null"))
+console.log(JSON.stringify({}))
+console.log(JSON.stringify([1,2,3]))
+console.log(JSON.stringify(19))
+console.log(JSON.stringify(null))
+console.log(JSON.stringify(undefined))
+console.log(JSON.stringify(true))
+console.log(JSON.stringify({x:2, y:3}))
+console.log(JSON.stringify(json))
